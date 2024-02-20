a8770036a76e4781c3daf6b27fbaf221bbb1734e
alex-johnson41 <alex@heathjohnson.org>
2024-02-20 15:16:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from ai_model.gemini_model import GeminiModel
-
-"""" 
-Random notes:
-    Create two instances of the model, one for creating the story,
-        and one for analyzing user inputs to determine if any dice rolls are needed for that action.
-    
-"""
+from ai_model.model_settings import SKILL_CHECK_PROMPT, STORY_PROMPT
+from character_creation.character_creator import CharacterCreator
+from game.game import Game
 
 
-model = GeminiModel()
-while True:
-    prompt = input("Enter a prompt:")
-    response = model.communicate(prompt)
-    print(response)
+def basic_communication():
+    model = GeminiModel(SKILL_CHECK_PROMPT)
+    while True:
+        prompt = input("Enter a prompt:")
+        response = model.communicate(prompt)
+        print(response)
+
+def play():
+    story_model = GeminiModel(STORY_PROMPT)
+    skill_model = GeminiModel(SKILL_CHECK_PROMPT)
+    player = CharacterCreator.create_character()
+    game = Game(player, story_model, skill_model)
+    game.play()
+
+basic_communication()
