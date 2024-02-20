6294fd77f84de8cf4f4586e4811b76de576a9b9c
alex-johnson41 <alex@heathjohnson.org>
2024-02-20 15:15:41 -0600

    Add model settings, remove filters on AI Model

--- ai_model/gemini_model.py
+++ ai_model/gemini_model.py
@@ -1,14 +1,17 @@
 import google.generativeai as genai
 import config
 from ai_model.abstract_ai_model import AbstractAIModel
+from .model_settings import SAFETY_SETTINGS
 
 
 class GeminiModel(AbstractAIModel):
-    def __init__(self):
+    def __init__(self, initialization_prompt: str = None):
         genai.configure(api_key=config.GEMINI_API_KEY)
-        self.model = genai.GenerativeModel('gemini-pro')
+        self.model = genai.GenerativeModel('gemini-pro', safety_settings=SAFETY_SETTINGS)
         self.chat = self.model.start_chat(history=[])
+        if initialization_prompt:
+            self.communicate(initialization_prompt)
 
     def communicate(self, prompt: str) -> str:
-        response = self.chat.send_message(prompt)
+        response = self.chat.send_message(prompt, safety_settings=SAFETY_SETTINGS)
         return response.text
---
+++ ai_model/model_settings.py
@@ -0,0 +1,36 @@
+SKILL_CHECK_PROMPT = """
+    You are going to return a json list, and nothing else. I am going to give you an action 
+    I want to make in my dungeons and dragons game, and you are going to return a json list of 
+    all the skill checks that need to be performed. Your options for skills are: 'Acrobatics', 
+    'Animal Handling', 'Arcana', 'Athletics', 'Deception', 'History', 'Insight', 'Intimidation',
+    'Investigation', 'Medicine', 'Nature', 'Perception', 'Performance', 'Persuasion', 'Religion',
+    'Sleight of Hand', 'Stealth', 'Survival'.
+    Only return the name of the skill, like so: [\"Perception\", \"Stealth\", \"Arcana\"]. 
+    Return an empty list if no skill checks are absoluteley needed.
+    Only return a skill if it is absolutely needed for the action.
+"""
+STORY_PROMPT = """
+    You are a dungeon master for a dungeons and dragons game. 
+"""
+SAFETY_SETTINGS = [
+        {
+            "category": "HARM_CATEGORY_DANGEROUS",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE",
+        },
+    ]
