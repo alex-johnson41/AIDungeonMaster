205cc1a74e2a2b897e5fcee53de4567a224fbbad
alex-johnson41 <alex@heathjohnson.org>
2024-04-02 16:41:49 -0500

    Fix input

--- logger.py
+++ logger.py
@@ -15,15 +15,16 @@ class Logger:
     
     # Use to get input from the player
     def input(self, message: str) -> str:
-        if message == "/help":
+        response = input(message)
+        if response == "/help":
             self.log("Commands: ")
             self.log("/exit - Exits the game")
             self.log("/debug - Toggles debug mode")
             self.log("/inventory - Displays your inventory (only available in game)")
             self.log("/help - Displays this message")
-        elif message == "/exit":
+        elif response == "/exit":
             exit()
-        elif message == "/debug":
+        elif response == "/debug":
             self.debug = not self.debug
             self.log(f"Debug mode set to: {self.debug}")
-        return input(message)
+        return response
