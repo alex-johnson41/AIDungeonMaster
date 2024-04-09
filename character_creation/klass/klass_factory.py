e54ee2f4f85b2d868bc663213936760c3fea4d24
alex-johnson41 <alex@heathjohnson.org>
2024-04-09 14:05:36 -0500

    Add new klasses and races to factories

--- character_creation/klass/klass_factory.py
+++ character_creation/klass/klass_factory.py
@@ -1,10 +1,16 @@
 from character_creation.klass.abstract_klass import AbstractKlass
 from character_creation.klass.fighter import Fighter
+from character_creation.klass.rogue import Rogue
+from character_creation.klass.wizard import Wizard
 
 
 class KlassFactory():
     def create_klass(self, klass_name: str) -> AbstractKlass:
         if klass_name == "Fighter":
             return Fighter()
+        elif klass_name == "Wizard":
+            return Wizard()
+        elif klass_name == "Rogue":
+            return Rogue()
         else:
             raise ValueError("Invalid class name")
