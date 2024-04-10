e535e00bf4f6f8c43a6b1295ca1b6c15bdad6d76
James <grillj@uni.edu>
2024-04-10 09:50:42 -0500

    Race Factory and Klass Factory modifications

--- character_creation/klass/klass_factory.py
+++ character_creation/klass/klass_factory.py
@@ -1,4 +1,5 @@
 from character_creation.klass.abstract_klass import AbstractKlass
+from character_creation.klass.cleric import Cleric
 from character_creation.klass.fighter import Fighter
 from character_creation.klass.rogue import Rogue
 from character_creation.klass.wizard import Wizard
@@ -12,5 +13,7 @@ class KlassFactory():
             return Wizard()
         elif klass_name == "Rogue":
             return Rogue()
+        elif klass_name == "Cleric":
+            return Cleric()
         else:
             raise ValueError("Invalid class name")
